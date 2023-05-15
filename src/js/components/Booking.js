4652efa44a8345e4ee35ac15b2b8cbd4a2ade4f9
Krzysztof Laudanski <laudanski.krzysztof86@gmail.com>
2023-05-15 12:32:09 +0200

    add api booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -49,26 +49,34 @@ class Booking {
 
         //console.log('getData urls', urls);
 
-        fetch(urls.booking)
+       /* fetch(urls.booking)
             .then(function (bookingsResponse) {
                 return bookingsResponse.json();
             })
             .then(function (bookings) {
                 console.log(bookings);
-            });
+            });*/
 
-        /*Promise.all([
+        Promise.all([
             fetch(urls.booking),
+            fetch(urls.eventsCurrent),
+            fetch(urls.eventsRepeat),
         ])
             .then(function (allResponses) {
                 const bookingsResponse = allResponses[0];
+                const eventsCurrentResponse = allResponses[1];
+                const eventsRepeatResponse = allResponses[2];
                 return Promise.all([
                     bookingsResponse.json(),
+                    eventsCurrentResponse.json(),
+                    eventsRepeatResponse.json(),
                 ]);
             })
-            .then(function ([bookings]) {
-                console.log(bookings);
-            });*/
+            .then(function ([bookings, eventsCurrent, eventsRepeat]) {
+                //console.log(bookings);
+                //console.log(eventsCurrent);
+                //console.log(eventsRepeat);
+            });
 
 
     }
